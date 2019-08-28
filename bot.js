6fc5cf2292068a81724ef8ad77ddfe3b30933cfb
riooo12 <53498786+riooo12@users.noreply.github.com>
2019-08-28 23:06:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
-const prefix = ['-'];
+const myid = ['392353149584932879'];
+const prefix = ['$'];
 
 client.on('ready', () => {
    console.log(`----------------`);
